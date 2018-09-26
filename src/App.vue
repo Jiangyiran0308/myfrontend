1e2472e28e26410dc9f52eedbdc336cd73d1fa9b
Jiangyiran0308 <35513643+Jiangyiran0308@users.noreply.github.com>
2018-09-26 10:45:30 +0800

    vue cli + element ui

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+  import Index from './views/home/index'
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/home/index.vue
@@ -0,0 +1,26 @@
+<style scoped>
+
+
+</style>
+<template>
+  <div id = "app">
+    <div :class=""></div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+  import './appFrame.css'
+  import 'element-ui'
+
+    export default {
+        name: "index",
+        components: {},
+        data() {
+            return {}
+        },
+        mounted() {
+        },
+        methods: {},
+    }
+</script>
